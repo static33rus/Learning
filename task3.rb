5eecbdf1c2844bf41a43658ae9d2cca2fa725e65
Mikhail <m.pavlov@ecotelecom.ru>
2018-05-30 12:04:40 +0300

    add zadanie3

---
+++ task3.rb
@@ -0,0 +1,16 @@
+t1, t2 = ARGV
+t1=Integer(t1)
+t2=Integer(t2)
+sum=t1+t2
+#Создадим функцию, которая будет возвращать целое от деления и остаток. С помощью нее будем переводить сек в минуты, минуты в часы, часы в дни и тд
+def get_norm_date(number, k)
+    ostatok=number%k
+    celoe=(number-ostatok)/k
+    return ostatok, celoe
+end
+# С помощью функции переведем в часы, минуты и секунды. Можно и в часы, но я закомментил, так как в задании это не требуется
+sec, min = get_norm_date(sum,60)
+min, hours = get_norm_date(min,60)
+#hours, days = get_norm_date(hours,24)
+puts "#{hours.to_s} час #{min.to_s} минут #{min.to_s} секунд"
+
