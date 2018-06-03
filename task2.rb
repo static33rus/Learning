7cf98a29a4da37cdf073cf13101ed12b57e607cc
static33rus <pro100mikhail@gmail.com>
2018-06-03 18:09:20 +0000

    Вторая задача

---
+++ task2.rb
@@ -0,0 +1,10 @@
+require 'unicode'
+day, month, year = ARGV
+month=Unicode::downcase(month)
+month_list=[" ","января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"]
+#Определяем номер месяца
+month_number=month_list.index(month)
+t1=Time.mktime(year,month_number,day)
+t2=Time.mktime(year.to_i+1,1,1)
+t3=(t2-t1)/60/60/24-1
+puts t3.round(0)
